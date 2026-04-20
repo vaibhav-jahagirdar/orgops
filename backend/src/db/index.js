acb9c015e2ec8aac56414786f8d635d6a11c744c
vaibhav-jahagirdhar <vaibhavj.22.beee@acharya.ac.in>
2026-04-20 16:22:50 +0530

    fix: use DATABASE_URL

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,15 +1,10 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-});
-
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
